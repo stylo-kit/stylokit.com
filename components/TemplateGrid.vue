bfbb66e5acfcea0358ffd5240db9969d89e0a97f
Acepmon <dtsogtbayar123@gmail.com>
2024-09-16 10:58:10 +0800

    skeleton loader added to cards

--- components/TemplateGrid.vue
+++ components/TemplateGrid.vue
@@ -1,21 +1,35 @@
 <script setup lang="ts">
-import type { Template } from '~/types';
-import type { PropType } from 'vue';
+  import type { Template } from "~/types";
+  import type { PropType } from "vue";
 
-defineProps({
-  items: {
-    required: true,
-    type: Array as PropType<Template[]>,
-  }
-});
+  defineProps({
+    items: {
+      required: true,
+      type: Array as PropType<Template[]>,
+    },
+    loading: {
+      required: false,
+      type: Boolean,
+      default: false
+    }
+  });
 </script>
 
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[24px]">
-    <TemplateCard
-      v-for="template in items"
-      :key="template.id"
-      :template="template"
-    />
+    <template v-if="loading">
+      <div class="flex flex-col relative group gap-[12px]" v-for="i in 8">
+        <div class="w-full h-[290px] bg-[rgba(9,9,11,0.4)] border border-white/5 rounded animate-pulse"></div>
+        <div class="w-[150px] h-[20px] bg-[rgba(9,9,11,0.4)] border border-white/5 rounded animate-pulse"></div>
+        <div class="w-[80px] h-[20px] bg-[rgba(9,9,11,0.4)] border border-white/5 rounded animate-pulse"></div>
+      </div>
+    </template>
+    <template v-else>
+      <TemplateCard
+        v-for="template in items"
+        :key="template.id"
+        :template="template"
+      />
+    </template>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-import type { Template } from '~/types';
+  import type { Template } from "~/types";
 
-const { data: framerTemplates } = await useAPI<{data: Template[]}>('/categories/framer/templates');
-const { data: nuxtTemplates } = await useAPI<{data: Template[]}>('/categories/nuxt/templates');
-const { data: figmaTemplates } = await useAPI<{data: Template[]}>('/categories/figma/templates');
+  const { data: framerTemplates, status: framerTemplatesStatus } =
+    await useAPI<{ data: Template[] }>("/categories/framer/templates");
+  const { data: nuxtTemplates, status: nuxtTemplatesStatus } = await useAPI<{
+    data: Template[];
+  }>("/categories/nuxt/templates");
+  const { data: figmaTemplates, status: figmaTemplatesStatus } = await useAPI<{
+    data: Template[];
+  }>("/categories/figma/templates");
 </script>
 
 <template>
@@ -11,7 +16,12 @@ const { data: figmaTemplates } = await useAPI<{data: Template[]}>('/categories/f
     <header class="flex flex-row justify-between">
       <h1 class="heading-3 text-primary">Explore</h1>
       <div class="flex flex-row gap-3">
-        <Button @click="navigateTo('/all-access')" variant="neo-green" :rounded="false">All Access - $499</Button>
+        <Button
+          @click="navigateTo('/all-access')"
+          variant="neo-green"
+          :rounded="false"
+          >All Access - $499</Button
+        >
       </div>
     </header>
 
@@ -19,21 +29,45 @@ const { data: figmaTemplates } = await useAPI<{data: Template[]}>('/categories/f
 
     <ClientOnly>
       <HeadlessTabGroup>
-        <HeadlessTabList class="flex flex-row gap-[8px]" v-slot="{ selectedIndex }">
-          <TagTab icon="tabler:brand-framer" label="Framer" :active="selectedIndex == 0" />
-          <TagTab icon="tabler:brand-nuxt" label="Nuxt" :active="selectedIndex == 1" />
-          <TagTab icon="tabler:brand-figma" label="UI Kit" :active="selectedIndex == 2" />
+        <HeadlessTabList
+          class="flex flex-row gap-[8px]"
+          v-slot="{ selectedIndex }"
+        >
+          <TagTab
+            icon="tabler:brand-framer"
+            label="Framer"
+            :active="selectedIndex == 0"
+          />
+          <TagTab
+            icon="tabler:brand-nuxt"
+            label="Nuxt"
+            :active="selectedIndex == 1"
+          />
+          <TagTab
+            icon="tabler:brand-figma"
+            label="UI Kit"
+            :active="selectedIndex == 2"
+          />
         </HeadlessTabList>
 
         <HeadlessTabPanels>
           <HeadlessTabPanel>
-            <TemplateGrid :items="framerTemplates.data" />
+            <TemplateGrid
+              :items="framerTemplates.data"
+              :loading="framerTemplatesStatus == 'pending'"
+            />
           </HeadlessTabPanel>
           <HeadlessTabPanel>
-            <TemplateGrid :items="nuxtTemplates.data" />
+            <TemplateGrid
+              :items="nuxtTemplates.data"
+              :loading="nuxtTemplatesStatus == 'pending'"
+            />
           </HeadlessTabPanel>
           <HeadlessTabPanel>
-            <TemplateGrid :items="figmaTemplates.data" />
+            <TemplateGrid
+              :items="figmaTemplates.data"
+              :loading="figmaTemplatesStatus == 'pending'"
+            />
           </HeadlessTabPanel>
         </HeadlessTabPanels>
       </HeadlessTabGroup>
