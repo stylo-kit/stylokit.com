be953b70ddef620874eb965d0054189d6a79637d
Acepmon <dtsogtbayar123@gmail.com>
2024-11-15 13:45:06 +0800

    nuxt content module added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,50 +1,25 @@
-import axios from 'axios';
-
-// async function getTemplateRoutes() {
-//   const { data } = await axios.get('https://admin.stylokit.com/api/templates')
-//   return data.data.map((template: any) => `/templates/${template.slug}`)
-// }
-
-const getTemplateRoutes = async () => {
-  const response = await axios.get(
-    'https://admin.stylokit.com/api/templates'
-  );
-  // return the array of routes
-  return response?.data?.data.map((template: any) => `/templates/${template.slug}`);
-};
+import axios from "axios";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  hooks: {
-    async 'nitro:config'(nitroConfig) {
-      // fetch the routes from our function above
-      const slugs = await getTemplateRoutes();
-      // add the routes to the nitro config
-      nitroConfig.prerender.routes.push(...slugs);
-    },
-  },
-  routeRules: {
-    // '/**': { isr: 60 },
-    "/templates/**": { prerender: true },
-    // "/blog/**": { isr: false }
-  },
-  ssr: false,
-  css: ['~/assets/css/main.css'],
+  ssr: true,
+  css: ["~/assets/css/main.css"],
   modules: [
-    'nuxt-headlessui',
-    'nuxt-gtag',
+    "nuxt-headlessui",
+    "nuxt-gtag",
     "vue3-carousel-nuxt",
-    '@nuxtjs/google-fonts',
+    "@nuxtjs/google-fonts",
+    "@nuxt/content",
   ],
   googleFonts: {
     families: {
       Roboto: true,
-      InstrumentSans: true
-    }
+      InstrumentSans: true,
+    },
   },
   headlessui: {
-    prefix: 'Headless'
+    prefix: "Headless",
   },
   postcss: {
     plugins: {
@@ -53,18 +28,18 @@ export default defineNuxtConfig({
     },
   },
   app: {
-    pageTransition: { name: 'page-fade', mode: 'out-in' }
+    pageTransition: { name: "page-fade", mode: "out-in" },
   },
   runtimeConfig: {
     public: {
-      apiBase: 'https://admin.stylokit.com/api'
-    }
+      apiBase: "https://admin.stylokit.com/api",
+    },
   },
   gtag: {
-    id: 'G-KG5CCDHTBM'
+    id: "G-KG5CCDHTBM",
   },
   carousel: {
-    prefix: 'Slideshow'
+    prefix: "Slideshow",
   },
-  compatibilityDate: '2024-09-13'
-})
+  compatibilityDate: "2024-09-13",
+});
