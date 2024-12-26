02457efcfd95f6afafe0d29cd30ee006dee8f36e
Acepmon <dtsogtbayar123@gmail.com>
2024-12-26 13:09:52 +0800

    badge updated with 'gift' label

--- components/Badge.vue
+++ components/Badge.vue
@@ -5,28 +5,34 @@
 </template>
 
 <script setup lang="ts">
-const props = defineProps({
-  variant: {
-    required: false,
-    type: String,
-    default: 'brand', // brand, gray, square, green, strong-green, purple, neo,
-  },
-  size: {
-    required: false,
-    type: String,
-    default: 'sm', // xs, sm
-  },
-});
+  const props = defineProps({
+    variant: {
+      required: false,
+      type: String,
+      default: "brand", // brand, gray, square, green, strong-green, purple, neo, red
+    },
+    size: {
+      required: false,
+      type: String,
+      default: "sm", // xs, sm
+    },
+  });
 
-const badgeClass = ref({
-  'pt-[5px] pb-[3px] px-[8px] rounded-full heading-7 font-medium': props.size == 'sm' && props.variant != 'square',
-  'py-1 px-2 rounded-full body-4 font-semibold': props.size == 'xs' && props.variant != 'square',
-  'py-1 px-4 rounded-none body-2 bg-brand-4 text-primary': props.variant == 'square',
-  'badge-brand-fill border border-brand-5 text-brand-5': props.variant == 'brand',
-  'bg-zing-700 text-zing-400': props.variant == 'gray',
-  'badge-green-fill badge-green-color': props.variant == 'green',
-  'badge-strong-green-fill badge-strong-green-color': props.variant == 'strong-green',
-  'badge-purple-fill text-purple-400': props.variant == 'purple',
-  'badge-neo-fill text-neo-green-500': props.variant == 'neo',
-});
+  const badgeClass = ref({
+    "pt-[5px] pb-[3px] px-[8px] rounded-full heading-7 font-medium":
+      props.size == "sm" && props.variant != "square",
+    "py-1 px-2 rounded-full body-4 font-semibold":
+      props.size == "xs" && props.variant != "square",
+    "py-1 px-4 rounded-none body-2 bg-brand-4 text-primary":
+      props.variant == "square",
+    "badge-brand-fill border border-brand-5 text-brand-5":
+      props.variant == "brand",
+    "bg-zing-700 text-zing-400": props.variant == "gray",
+    "badge-green-fill badge-green-color": props.variant == "green",
+    "badge-strong-green-fill badge-strong-green-color":
+      props.variant == "strong-green",
+    "badge-purple-fill text-purple-400": props.variant == "purple",
+    "badge-neo-fill text-neo-green-500": props.variant == "neo",
+    "badge-red-fill badge-red-color": props.variant == "red",
+  });
 </script>
--- components/TemplateCard.vue
+++ components/TemplateCard.vue
@@ -1,24 +1,49 @@
 <script setup lang="ts">
-import type { Template } from '~/types';
-defineProps({
-  template: {
-    required: true,
-    type: Object as PropType<Template>,
-  }
-});
+  import type { Template } from "~/types";
+  defineProps({
+    template: {
+      required: true,
+      type: Object as PropType<Template>,
+    },
+  });
 </script>
 
 <template>
-  <NuxtLink :to="'/templates/' + template.slug" class="flex flex-col relative group gap-[12px]">
-    <div class="relative transition group-hover:-translate-y-1 duration-300 w-full h-[208px] md:h-auto overflow-hidden">
-      <img v-if="template.thumbImage" :src="template.thumbImage" alt="Image" class="w-full h-[208px] md:h-auto object-cover object-center rounded-md border border-white/10" />
-      <div @click="navigateTo(template.previewLink, {
-        external: true, open: {
-          target: '_blank'
-        }
-      })" class="flex items-center justify-center p-[6px] rounded-md template-card-icon-button-bg absolute top-4 right-4 opacity-0 group-hover:opacity-100 transition duration-300 group-hover:translate-x-1 group-hover:-translate-y-1">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-[20px] text-white font-regular">
-          <path fill-rule="evenodd" d="M8.25 3.75H19.5a.75.75 0 0 1 .75.75v11.25a.75.75 0 0 1-1.5 0V6.31L5.03 20.03a.75.75 0 0 1-1.06-1.06L17.69 5.25H8.25a.75.75 0 0 1 0-1.5Z" clip-rule="evenodd" />
+  <NuxtLink
+    :to="'/templates/' + template.slug"
+    class="flex flex-col relative group gap-[12px]"
+  >
+    <div
+      class="relative transition group-hover:-translate-y-1 duration-300 w-full h-[208px] md:h-auto overflow-hidden"
+    >
+      <img
+        v-if="template.thumbImage"
+        :src="template.thumbImage"
+        alt="Image"
+        class="w-full h-[208px] md:h-auto object-cover object-center rounded-md border border-white/10"
+      />
+      <div
+        @click="
+          navigateTo(template.previewLink, {
+            external: true,
+            open: {
+              target: '_blank',
+            },
+          })
+        "
+        class="flex items-center justify-center p-[6px] rounded-md template-card-icon-button-bg absolute top-4 right-4 opacity-0 group-hover:opacity-100 transition duration-300 group-hover:translate-x-1 group-hover:-translate-y-1"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="size-[20px] text-white font-regular"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8.25 3.75H19.5a.75.75 0 0 1 .75.75v11.25a.75.75 0 0 1-1.5 0V6.31L5.03 20.03a.75.75 0 0 1-1.06-1.06L17.69 5.25H8.25a.75.75 0 0 1 0-1.5Z"
+            clip-rule="evenodd"
+          />
         </svg>
       </div>
     </div>
@@ -26,21 +51,40 @@ defineProps({
       <div class="flex flex-row justify-between items-center">
         <div class="flex flex-row gap-[8px] items-center justify-center">
           <h5 class="heading-5 text-white">{{ template.name }}</h5>
-          <Badge v-for="tag in template.tags" :variant="tag.toLowerCase() == 'popular' ? 'neo' : tag.toLocaleLowerCase() == 'new' ? 'green' : 'gray'" size="xs">{{ tag }}</Badge>
+          <Badge
+            v-for="tag in template.tags"
+            :variant="
+              tag.toLowerCase() == 'popular'
+                ? 'neo'
+                : tag.toLowerCase() == 'new'
+                ? 'green'
+                : tag.toLowerCase() == 'gift'
+                ? 'red'
+                : 'gray'
+            "
+            size="xs"
+            >{{ tag }}</Badge
+          >
         </div>
-        <span v-if="template.price > 0" class="body-2 text-zing-500">${{ template.price }}</span>
+        <span v-if="template.price > 0" class="body-2 text-zing-500"
+          >${{ template.price }}</span
+        >
         <span v-else class="body-2 text-purple-400">Free</span>
       </div>
-      <div class="flex flex-row justify-start items-center flex-nowrap gap-[8px]">
-        <span class="body-2 text-zing-500 capitalize">{{ template.category }}</span>
+      <div
+        class="flex flex-row justify-start items-center flex-nowrap gap-[8px]"
+      >
+        <span class="body-2 text-zing-500 capitalize">{{
+          template.category
+        }}</span>
       </div>
     </div>
   </NuxtLink>
 </template>
 
 <style scoped>
-.template-card-icon-button-bg {
-  background: rgba(9, 9, 11, 0.4);
-  backdrop-filter: blur(8px);
-}
+  .template-card-icon-button-bg {
+    background: rgba(9, 9, 11, 0.4);
+    backdrop-filter: blur(8px);
+  }
 </style>
